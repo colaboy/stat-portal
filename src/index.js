b46e85d59ea9fc7ca0b2f2b8bd39d55be83833f1
wangmingzhu <wangmingzhu@waiqin365.com>
2018-05-28 09:14:28 +0800

    polyfill map and set

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import 'core-js/es6/map'; // 兼容es6的Map类
-// import 'core-js/es6/set'; // 兼容es6的Set类
+import 'core-js/es6/map'; // 兼容es6的Map类
+import 'core-js/es6/set'; // 兼容es6的Set类
 // import 'raf/polyfill'; // 兼容requestAnimationFrame动画
 import React from 'react';
 import ReactDOM from 'react-dom';
